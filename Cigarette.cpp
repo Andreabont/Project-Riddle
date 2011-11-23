50d523a4b2a15f86dc9b89dca1b3a5576a56a904
Andreabont <andreabont@gmail.com>
2011-11-23 14:13:44 +0100

    Update 23/11/2011 14:13

--- Cigarette.cpp
+++ Cigarette.cpp
@@ -23,6 +23,9 @@ int main(int argc, char **argv) {
 	options_description desc("Cigarette - Network Packet Parser");
 	desc.add_options()
 		("help", "prints this")
+		("ipv4", "show only IPv4 Packets")
+		("ipv6", "show only IPv6 Packets")
+		("arp", "show only ARP Packets")
 	;
 
 	variables_map vm;
@@ -43,10 +46,23 @@ int main(int argc, char **argv) {
 		getline(cin,line);
 		if(cin.eof()) break;
 		etherhead = parseEthernet(line,line.length());
-		cout<<"---- Packet ("<<dec<<line.length()<<" byte)"<<endl;
-		cout<<"EtherAddr | "<<etherhead.mac_src<<" --> "<<etherhead.mac_dst<<endl;
-		cout<<"EtherType | 0x"<<hex<<etherhead.ether_type<<" ("<<ether_type_decode(etherhead.ether_type)<<")"<<endl;
-		cout<<endl;
+		
+		int flag = 0;
+		
+		if(vm.count("arp") || vm.count("ipv4") || vm.count("ipv6"))
+		{
+			if(vm.count("arp") && etherhead.ether_type == ETHER_TYPE_ARP) flag = 1;
+			if(vm.count("ipv4") && etherhead.ether_type == ETHER_TYPE_IPV4) flag = 1;
+			if(vm.count("ipv6") && etherhead.ether_type == ETHER_TYPE_IPV6) flag = 1;
+		} else flag = 1;
+		
+		if(flag)
+		{
+			cout<<"---- Packet ("<<dec<<line.length()<<" byte)"<<endl;
+			cout<<"EtherAddr | "<<etherhead.mac_src<<" --> "<<etherhead.mac_dst<<endl;
+			cout<<"EtherType | 0x"<<hex<<etherhead.ether_type<<" ("<<ether_type_decode(etherhead.ether_type)<<")"<<endl;
+			cout<<endl;
+		}
 	}
 
 	return EXIT_SUCCESS;
