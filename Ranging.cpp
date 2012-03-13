28e806c2f21dc7de942bc9320d84ec61f0b31bf9
Andrea Bontempi <andreabont@gmail.com>
2012-03-13 17:50:26 +0100

    ranging++

--- Ranging.cpp
+++ Ranging.cpp
@@ -14,18 +14,27 @@
 #include <cstdlib>
 #include <string>
 #include <iomanip>
+#include <list>
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
+
+#include <curses.h>
 #include "libAddress.h"
 #include "libPacket.h"
+#include "libRanging.h"
 
+int rows; // number of rows in window
+int cols; // number of columns in window
 
 using namespace std;
 using namespace boost;
 using namespace boost::program_options;
+
+void setHead();
+void printLine(int countLine, string mac, string ip);
 
 int main(int argc, char **argv) {
 
@@ -41,9 +50,30 @@ int main(int argc, char **argv) {
     if (vm.count("help"))
     {
         cout<<desc<<"\n";
-        return 1;
+        return EXIT_SUCCESS;
     }
-  
+
+    WINDOW *wnd;
+
+    wnd = initscr();	// curses call to initialize window
+
+    if (has_colors() == FALSE)
+    {
+        endwin();
+        cerr << "FAIL: Your terminal does not support color." << endl;
+        return EXIT_FAILURE;
+    }
+
+    start_color();                          // start color mode.
+    cbreak();                               // curses call to set no waiting for Enter key
+    noecho();                               // curses call to set no echoin
+    clear();                                // curses call to clear screen, send cursor to position (0,0)
+
+    getmaxyx(wnd, rows, cols);
+    setHead();
+
+    list<device> found;
+
     while (1)
     {
         try
@@ -56,21 +86,143 @@ int main(int argc, char **argv) {
             boost::algorithm::split(line, r_packet, boost::algorithm::is_any_of("!"));
 
             packet* pkg = packet::factory(lexical_cast<int>(line[0]), lexical_cast<int>(line[1]), line[2]);
-	    
-	    if(pkg->isArp())
-	    {
-		ARPpacket *pkg_arp = dynamic_cast<ARPpacket*>(pkg);
-                cout << pkg_arp->getSenderMac().to_string() << " is at "<< pkg_arp->getSenderIp().to_string() << endl;	      
-	    }
 
-	    delete pkg;
-	    
+            if(pkg->isArp())
+            {
+                ARPpacket *pkg_arp = dynamic_cast<ARPpacket*>(pkg);
+
+                list<device>::iterator p = found.begin();
+
+                bool isFound = false;
+
+                while(p != found.end()) {
+
+                    if(p->getMacAddress() == pkg_arp->getSenderMac() && p->getIpAddress() == pkg_arp->getSenderIp())
+                    {
+                        p->setEpoch(pkg_arp->getEpoch());
+                        isFound = true;
+                        break;
+                    }
+
+                    p++;
+                }
+
+                if(!isFound)
+                {
+                    device newDevice(pkg_arp->getSenderMac(), pkg_arp->getSenderIp(), pkg_arp->getEpoch());
+                    found.push_back(newDevice);
+                }
+            }
+
+            delete pkg;
+
         }
         catch (packet::Overflow)
         {
-            std::cerr<<"Overflow! :-P"<<std::endl;
+            cerr << "Overflow! :-P" << endl;
+            endwin();
             return EXIT_FAILURE;
         }
+
+
+        setHead();
+
+        int countLine = 1;
+
+        list<device>::iterator q = found.begin();
+        while(q != found.end()) {
+
+            printLine(countLine, q->getMacAddress().to_string(), q->getIpAddress().to_string());
+
+            countLine++;
+            q++;
+        }
+
+
     }
+    endwin();
     return EXIT_SUCCESS;
 }
+
+void setHead()
+{
+
+    char *head;
+    int ind1;
+    int ind2;
+
+    if(head = (char*)malloc(cols * sizeof(char)))
+    {
+        snprintf(head, cols, "Ranging - I found these devices:");
+
+        ind2 = strlen(head);
+
+        for (ind1 = ind2; ind1 < cols; ind1++)
+        {
+            head[ind1] = (int)' ';
+        }
+
+        head[cols] = (int)'\0';
+    }
+    else
+    {
+        printf("FAIL: Memory Allocation Failure\n");
+        return;
+    }
+
+    move(0, 0);
+
+    init_pair(1, COLOR_WHITE, COLOR_RED);
+
+    attron(COLOR_PAIR(1));	// set color for title
+
+    addstr(head);
+
+    free(head);
+
+    refresh();
+
+    return;
+}
+
+void printLine(int countLine, string mac, string ip)
+{
+
+    char *head;
+    int ind1;
+    int ind2;
+
+    if(head = (char*)malloc(cols * sizeof(char)))
+    {
+        snprintf(head, cols, "%s is at %s", mac.c_str(), ip.c_str());
+
+        ind2 = strlen(head);
+
+        for (ind1 = ind2; ind1 < cols; ind1++)
+        {
+            head[ind1] = (int)' ';
+        }
+
+        head[cols] = (int)'\0';
+    }
+    else
+    {
+        printf("FAIL: Memory Allocation Failure\n");
+        return;
+    }
+
+    move(countLine, 0);
+
+    init_pair(2, COLOR_WHITE, COLOR_BLACK);
+
+    attron(COLOR_PAIR(2));	// set color for title
+
+    addstr(head);
+
+    free(head);
+
+    refresh();
+
+    return;
+}
+
